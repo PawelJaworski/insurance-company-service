739e5e9a5ef8ec6934fac77d055e5448a6031df8
Pawel Jaworski <pawel.jaworski-extern@dbschenker.com>
2026-08-24 20:00:42 +0200

    agents fix

--- src/test/java/pl/pjaworski/insurance_company/domain/policy/SequentialPolicyNumberGeneratorAbility.java
+++
@@ -1,9 +0,0 @@
-package pl.pjaworski.insurance_company.domain.policy;
-
-public interface SequentialPolicyNumberGeneratorAbility {
-    PolicyNumberGenerator INSTANCE = new SequentialPolicyNumberGenerator();
-
-    default PolicyNumberGenerator getPolicyNumberGenerator() {
-        return INSTANCE;
-    }
-}
